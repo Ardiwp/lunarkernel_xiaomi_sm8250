adcb3f9fb94e52773f43f6837a8578867fa7252b
Cloud_Yun <1770669041@qq.com>
2025-06-08 21:50:32 +0900

    lz4: 更新 LZ4 至 1.10.0 版本

--- crypto/lz4hc.c
+++ crypto/lz4hc.c
@@ -97,11 +97,7 @@ static int __lz4hc_decompress_crypto(const u8 *src, unsigned int slen,
 {
 	int out_len;
 
-#if defined(CONFIG_ARM64) && defined(CONFIG_KERNEL_MODE_NEON)
-	out_len = LZ4_arm64_decompress_safe(src, dst, slen, *dlen, false);
-#else
 	out_len = LZ4_decompress_safe(src, dst, slen, *dlen);
-#endif
 
 	if (out_len < 0)
 		return -EINVAL;
