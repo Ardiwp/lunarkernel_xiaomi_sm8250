50f1ba93f0738b5fccb6756cdfa8af49e6f4cb9a
Cloud_Yun <1770669041@qq.com>
2025-06-14 23:07:42 +0900

    Revert "sysctl: Rename swappiness"
    This reverts commit f9196761fc774bb3a7b3f96eb14668c3568c6280.

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -1496,7 +1496,7 @@ static struct ctl_table vm_table[] = {
 		.extra1		= &zero,
 	},
 	{
-		.procname	= "swappiness_nosys",
+		.procname	= "swappiness",
 		.data		= &vm_swappiness,
 		.maxlen		= sizeof(vm_swappiness),
 		.mode		= 0644,
